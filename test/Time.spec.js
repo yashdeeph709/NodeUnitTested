4688036ab5e4ccf2be39b2b3c801d5a8f2f5a44a
Yashdeep Hinge <yashdeeph709@gmail.com>
2016-02-04 12:00:05 +0530

    Boundary tests add and tests are passing

--- test/Time.spec.js
+++ test/Time.spec.js
@@ -11,6 +11,20 @@ describe("Time", function () {
             expect(time1).to.have.a.property("hours", 13);
             expect(time1).to.have.a.property("minutes", 42);
         });
+        it("should add minutes if 0 starts", function () {
+            var time1 = new Time("12:02");
+            var time2 = new Time("1:04");
+            time1.add(time2);
+            expect(time1).to.have.a.property("hours", 13);
+            expect(time1).to.have.a.property("minutes", 06);
+        });
+        it("should add minutes if cross min by 1", function () {
+            var time1 = new Time("12:58");
+            var time2 = new Time("1:04");
+            time1.add(time2);
+            expect(time1).to.have.a.property("hours", 14);
+            expect(time1).to.have.a.property("minutes", 02);
+        });
         it("should accept zero hours", function () {
             var time1 = new Time("00:22");
             var time2 = new Time("1");
